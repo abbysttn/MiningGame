827130362ebd9f66dc6ee2259ab4af99d707e439
samh812 <132234555+samh812@users.noreply.github.com>
2025-05-23 06:07:04 +0100

    Everything so far

---
+++ game.cpp
@@ -0,0 +1,216 @@
+// COMP710 GP Framework 2022
+// This include:
+#include "game.h"
+
+// Library includes:
+#include "renderer.h"
+#include "logmanager.h"
+#include "sprite.h"
+#include <iostream>
+#include "scenecheckerboards.h"
+#include "imgui/imgui_impl_sdl2.h"
+#include "inputsystem.h"
+#include "xboxcontroller.h"
+#include "fmod.hpp"
+
+// Static Members:
+Game* Game::sm_pInstance = 0;
+FMOD::System* m_pFMODSystem = nullptr;
+
+Game& Game::GetInstance()
+{
+	if (sm_pInstance == 0)
+	{
+
+		sm_pInstance = new Game();
+	}
+
+	return (*sm_pInstance);
+}
+
+void Game::DestroyInstance()
+{
+	delete sm_pInstance;
+	sm_pInstance = 0;
+}
+
+Game::Game() : m_pRenderer(0), m_bLooping(true)
+{
+
+}
+
+Game::~Game()
+{
+	delete m_pRenderer;
+	m_pRenderer = 0;
+}
+
+void Game::Quit()
+{
+	m_bLooping = false;
+}
+
+bool Game::Initialise()
+{
+	int bbWidth = 960;
+	int bbHeight = 720;
+
+	m_pRenderer = new Renderer();
+	if (!m_pRenderer->Initialise(true, bbWidth, bbHeight))
+	{
+		LogManager::GetInstance().Log("Renderer failed to initialise!");
+		return false;
+	}
+
+	bbWidth = m_pRenderer->GetWidth();
+	bbHeight = m_pRenderer->GetHeight();
+
+	m_iLastTime = SDL_GetPerformanceCounter();
+
+	m_pRenderer->SetClearColour(0, 255, 255);
+
+	// Initialise FMOD
+	FMOD::System_Create(&m_pFMODSystem);
+	m_pFMODSystem->init(512, FMOD_INIT_NORMAL, 0);
+
+	// Initialise Input System
+	m_pInputSystem= new InputSystem();
+	if (!m_pInputSystem->Initialise())
+	{
+		LogManager::GetInstance().Log("InputSystem failed to initialise!");
+		return false;
+	}
+
+	Scene* pScene = 0;
+	pScene = new SceneCheckerboards(m_pFMODSystem);
+	pScene->Initialise(*m_pRenderer);
+	m_scenes.push_back(pScene);
+
+	m_iCurrentScene = 0;
+
+	return true;
+}
+
+bool Game::DoGameLoop()
+{
+	const float stepSize = 1.0f / 60.0f;
+
+	// Process input 
+	m_pInputSystem->ProcessInput();
+
+	if (m_bLooping)
+	{
+		Uint64 current = SDL_GetPerformanceCounter();
+		float deltaTime = (current - m_iLastTime) / static_cast<float>(SDL_GetPerformanceFrequency());
+
+		m_iLastTime = current;
+
+		m_fExecutionTime += deltaTime;
+
+		Process(deltaTime);
+
+#ifdef USE_LAG
+		m_fLag += deltaTime;
+
+		int innerLag = 0;
+
+		while (m_fLag >= stepSize)
+		{
+			Process(stepSize);
+
+			m_fLag -= stepSize;
+
+			++m_iUpdateCount;
+			++innerLag;
+		}
+#endif //USE_LAG
+
+		Draw(*m_pRenderer);
+	}
+
+	return m_bLooping;
+}
+
+void Game::Process(float deltaTime)
+{
+	ProcessFrameCounting(deltaTime);
+
+	m_scenes[m_iCurrentScene]->Process(deltaTime, *m_pInputSystem);
+
+	// Toggle Debug Window
+	ButtonState backspaceState = m_pInputSystem->GetKeyState(SDL_SCANCODE_BACKSPACE);
+	if (backspaceState == BS_PRESSED)
+	{
+		std::cout << "Backspace pressed" << std::endl;
+		ToggleDebugWindow();
+	}
+
+	// Quit Game
+	ButtonState escapeState = m_pInputSystem->GetKeyState(SDL_SCANCODE_ESCAPE);
+	if (escapeState == BS_PRESSED)
+	{
+		std::cout << "Escape pressed" << std::endl;
+		Quit();
+	}
+}
+
+void Game::Draw(Renderer& renderer)
+{
+
+	++m_iFrameCount;
+
+	renderer.Clear();
+
+	//Draw Current Scene
+	m_scenes[m_iCurrentScene]->Draw(renderer);
+
+	DebugDraw();
+
+	renderer.Present();
+}
+
+void
+Game::ProcessFrameCounting(float deltaTime)
+{
+	// Count total simulation time elapsed:
+	m_fElapsedSeconds += deltaTime;
+
+	// Frame Counter:
+	if (m_fElapsedSeconds > 1.0f)
+	{
+		m_fElapsedSeconds -= 1.0f;
+		m_iFPS = m_iFrameCount;
+		m_iFrameCount = 0;
+	}
+}
+
+void Game::DebugDraw
+()
+{
+	if (m_bShowDebugWindow) {
+		bool open = true;
+
+		ImGui::Begin("Debug Window", &open, ImGuiWindowFlags_MenuBar);
+
+		ImGui::Text("COMP710 GP Framework (%s)", "2025, S1");
+
+		if (ImGui::Button("Quit"))
+		{
+			Quit();
+		}
+
+		ImGui::SliderInt("Active scene", &m_iCurrentScene, 0, m_scenes.size() - 1, "%d");
+
+		m_scenes[m_iCurrentScene]->DebugDraw();
+
+		ImGui::End();
+	}
+}
+
+void Game::ToggleDebugWindow
+()
+{
+	std::cout << "Toggle Debug Window" << std::endl;
+	m_bShowDebugWindow = !m_bShowDebugWindow;
+	m_pInputSystem->ShowMouseCursor(m_bShowDebugWindow);
+}
