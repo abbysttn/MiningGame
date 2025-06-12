0c488c1c0d2d6ec47811bcc891e92471a9347337
samh812 <132234555+samh812@users.noreply.github.com>
2025-06-12 15:07:26 +1200

    fixed last memory leak

--- GP2025/main.cpp
+++ GP2025/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 	Game& gameInstance = Game::GetInstance();
 
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	//_CrtSetBreakAlloc(256);
+	//_CrtSetBreakAlloc(280);
 
 	if (!gameInstance.Initialise())
 	{
